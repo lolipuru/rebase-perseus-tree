1e35c112028c85a62412a1b66f30d37e5dd48548
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-08 06:48:35 -0400

    sdm845-common: import libperfmgr from Google
    android-12.0.0_r2: hardware/google/pixel
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -332,10 +332,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.xiaomi-libperfmgr 
+    android.hardware.power-service.xiaomi-sdm845-libperfmgr \
+    android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Neural Network
 PRODUCT_PACKAGES += \
